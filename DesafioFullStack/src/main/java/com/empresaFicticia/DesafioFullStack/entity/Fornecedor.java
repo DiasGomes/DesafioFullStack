5ea27ff01a21d2425ace58612ebdb7b6302f6fc8
DiasGomes <jaovdg@gmail.com>
2025-08-31 19:27:50 -0300

    add rg e data nascimento

--- DesafioFullStack/src/main/java/com/empresaFicticia/DesafioFullStack/entity/Fornecedor.java
+++ DesafioFullStack/src/main/java/com/empresaFicticia/DesafioFullStack/entity/Fornecedor.java
@@ -1,5 +1,7 @@
 package com.empresaFicticia.DesafioFullStack.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -17,25 +19,35 @@ public class Fornecedor {
     private String email;
     @Column(nullable = false)
     private String cep;
+    @Column
+    private String rg;
+    @Column
+    private LocalDate dataNascimento;
 
     public Fornecedor() {}
 
-    public Fornecedor(Long id, String cpfCnpj, String nome, String email, String cep) {
+    public Fornecedor(Long id, String cpfCnpj, String nome, String email, String cep, String rg, LocalDate dataNascimento) {
         this.id = id;
         this.cpfCnpj = cpfCnpj;
         this.nome = nome;
         this.email = email;
         this.cep = cep;
+        this.rg = rg;
+        this.dataNascimento = dataNascimento;
     }
 
     public String getCpfCnpj() {return cpfCnpj;}
     public String getNome() {return nome;}
     public String getEmail() {return email;}
     public String getCEP() {return cep;}
+    public String getRg() {return rg;}
+    public LocalDate getDataNascimento() {return dataNascimento;}
 
     public void setCpfCnpj(String cpfCnpj) {this.cpfCnpj = cpfCnpj;}
     public void setNome(String nome) {this.nome = nome;}
     public void setEmail(String email){this.email = email;}
     public void setCEP(String cep) {this.cep = cep;}
+    public void setRg(String rg) {this.rg = rg;}
+    public void setDataNascimento(LocalDate dataNascimento) {this.dataNascimento = dataNascimento;}
 
 }
--- cria_base_dados.sql
+++ cria_base_dados.sql
@@ -5,16 +5,18 @@ USE `DB_Desafio_Full_Stack`;
 
 
 CREATE TABLE fornecedor (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   cpf_cnpj VARCHAR(14) UNIQUE NOT NULL,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   cep VARCHAR(10) NOT NULL,
+  rg VARCHAR(15),
+  data_nascimento DATE,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE empresa (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   cnpj VARCHAR(14) UNIQUE NOT NULL,
   nome_fantasia VARCHAR(50) NOT NULL,
   cep VARCHAR(10) NOT NULL,
@@ -22,17 +24,18 @@ CREATE TABLE empresa (
 );
 
 
-INSERT INTO fornecedor (id, cpf_cnpj, nome, email, cep)
+INSERT INTO fornecedor (id, cpf_cnpj, nome, email, cep, rg, data_nascimento)
 VALUES
-(0, '12345678901','Leslie', 'Leslie@g.com', '00111010'),
-(1, '12345678902', 'Andy', 'Andy@g.com', '10111011'),
-(2, '12345678903','Tom', 'Tom@g.com', '20111012'),
-(3, '12345678904', 'April', 'April@g.com', '30111013'),
-(4, '12345678905', 'Jerry', 'Jerry@g.com', '40111014'),
-(5, '12345678906', 'Donna', 'Donna@g.com', '50111015'),
-(6, '12345678907', 'Ana', 'Ana@g.com', '60111016'),
-(7, '12345678908', 'Chris', 'Chris@g.com', '70111017'),
-(8, '12345678909', 'Ben', 'Ben@g.com', '80111018');
+(1, '12345678902', 'Andy', 'Andy@g.com', '10111011', 'AA-11.222.333', STR_TO_DATE('31-08-2000', '%d-%m-%Y')),
+(2, '12345678903','Tom', 'Tom@g.com', '20111012', 'AA-22.222.333', STR_TO_DATE('01-03-1995', '%d-%m-%Y')),
+(3, '12345678904', 'April', 'April@g.com', '30111013', 'AA-33.222.333', STR_TO_DATE('03-10-1990', '%d-%m-%Y')),
+(4, '12345678905', 'Jerry', 'Jerry@g.com', '40111014', 'AA-44.222.333', STR_TO_DATE('11-02-2002', '%d-%m-%Y')),
+(5, '12345678906', 'Donna', 'Donna@g.com', '50111015', 'AA-55.222.333', STR_TO_DATE('29-12-2004', '%d-%m-%Y')),
+(6, '12345678907', 'Ana', 'Ana@g.com', '60111016', 'AA-66.222.333', STR_TO_DATE('17-10-1984', '%d-%m-%Y')),
+(7, '12345678908', 'Chris', 'Chris@g.com', '70111017', 'AA-77.222.333', STR_TO_DATE('07-07-1997', '%d-%m-%Y')),
+(8, '12345678901','Leslie', 'Leslie@g.com', '00111010', 'AA-88.222.333', STR_TO_DATE('14-09-2001', '%d-%m-%Y')),
+(9, '12345678909', 'Ben', 'Ben@g.com', '80111018', 'AA-99.222.333', STR_TO_DATE('19-11-2002', '%d-%m-%Y')),
+(10, '43210987654321', 'Leslie Corp', 'Leslie@Corp.com', '01234560', null, null);
 
 
 INSERT INTO empresa (id, cnpj, nome_fantasia, cep)
@@ -41,4 +44,5 @@ VALUES
 (2, '53210987654321', 'Ron Industry', '11234561'),
 (3, '63210987654321', 'Tom & Jerry', '21234562'),
 (4, '73210987654321', 'Ben 11', '31234563'),
-(5, '83210987654321', 'Andy Toys', '41234564');
+(5, '83210987654321', 'Andy Toys', '41234564'),
+(6, '93210987654321', 'a empresa do malvado doofenshmirtz', '51234565');
