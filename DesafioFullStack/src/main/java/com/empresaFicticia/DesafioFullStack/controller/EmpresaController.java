1cddfc6e9faef5ada2935d3d326141da495533ec
DiasGomes <jaovdg@gmail.com>
2025-09-02 12:07:53 -0300

    add vue router

--- DesafioFullStack/src/main/java/com/empresaFicticia/DesafioFullStack/controller/EmpresaController.java
+++ DesafioFullStack/src/main/java/com/empresaFicticia/DesafioFullStack/controller/EmpresaController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import com.empresaFicticia.DesafioFullStack.entity.Empresa;
+import com.empresaFicticia.DesafioFullStack.repository.EmpresaRepository;
 import com.empresaFicticia.DesafioFullStack.entity.Fornecedor;
 import com.empresaFicticia.DesafioFullStack.repository.FornecedorRepository;
 
@@ -17,11 +21,16 @@ import com.empresaFicticia.DesafioFullStack.repository.FornecedorRepository;
 public class EmpresaController {
 
     @Autowired
+    private EmpresaRepository empresaRepository;
+
+    @Autowired
     private FornecedorRepository fornecedorRepository;
 
     @GetMapping("/{id}")
-    public List<Fornecedor> getAllFornecedor() {
-        return fornecedorRepository.findAll();
+    public ResponseEntity<Empresa> getEmpresa(@PathVariable Long id) {
+        return empresaRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
